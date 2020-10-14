96242ca82326148de44257220eb921c6cb90185d
Malte Tölle <malte.toelle@gmail.com>
2020-10-14 16:40:16 +0200

    minor updates

--- modules/layers.py
+++ modules/layers.py
@@ -23,21 +23,18 @@ class LinearRT(RTLayer):
             kl_divergence_fn = kl_divergence
 
         if bias:
-            super(LinearRT, self).__init__(
-                weight_posterior=mean_field_normal_initializer((out_features, in_features), posterior["loc"], posterior["ro"]),
-                weight_prior=prior,
-                bias_posterior=mean_field_normal_initializer(out_features, posterior["loc"], posterior["ro"]),
-                bias_prior=prior,
-                kl_divergence_fn=kl_divergence_fn,
-                layer_fn=F.linear)
+            bias_posterior = mean_field_normal_initializer(out_features, posterior["loc"], posterior["ro"])
+            bias_prior = prior
         else:
-            super(LinearRT, self).__init__(
-                weight_posterior=mean_field_normal_initializer((out_features, in_features), posterior["loc"], posterior["ro"]),
-                weight_prior=prior,
-                bias_posterior=None,
-                bias_prior=None,
-                kl_divergence_fn=kl_divergence_fn,
-                layer_fn=F.linear)
+            bias_posterior, bias_prior = None, None
+            
+        super(LinearRT, self).__init__(
+            weight_posterior=mean_field_normal_initializer((out_features, in_features), posterior["loc"], posterior["ro"]),
+            weight_prior=prior,
+            bias_posterior=bias_posterior,
+            bias_prior=bias_prior,
+            kl_divergence_fn=kl_divergence_fn,
+            layer_fn=F.linear)
 
 class Conv2dRT(RTLayer):
 
@@ -64,29 +61,22 @@ class Conv2dRT(RTLayer):
             kl_divergence_fn = kl_divergence
 
         if bias:
-            super(Conv2dRT, self).__init__(
-                weight_posterior=mean_field_normal_initializer((out_channels, in_channels // groups, kernel_size[0], kernel_size[1]), posterior["loc"], posterior["ro"]),
-                weight_prior=prior,
-                bias_posterior=mean_field_normal_initializer(out_channels, posterior["loc"], posterior["ro"]),
-                bias_prior=prior,
-                kl_divergence_fn=kl_divergence_fn,
-                layer_fn=F.Conv2d,
-                stride=stride,
-                padding=padding,
-                dilation=dilation,
-                groups=groups)
+            bias_posterior = mean_field_normal_initializer(out_channels, posterior["loc"], posterior["ro"])
+            bias_prior = prior
         else:
-            super(Conv2dRT, self).__init__(
-                weight_posterior=mean_field_normal_initializer((out_channels, in_channels // groups, kernel_size[0], kernel_size[1]), posterior["loc"], posterior["ro"]),
-                weight_prior=prior,
-                bias_posterior=None,
-                bias_prior=None,
-                kl_divergence_fn=kl_divergence_fn,
-                layer_fn=F.Conv2d,
-                stride=stride,
-                padding=padding,
-                dilation=dilation,
-                groups=groups)
+            bias_posterior, bias_prior = None, None
+
+        super(Conv2dRT, self).__init__(
+            weight_posterior=mean_field_normal_initializer((out_channels, in_channels // groups, kernel_size[0], kernel_size[1]), posterior["loc"], posterior["ro"]),
+            weight_prior=prior,
+            bias_posterior=bias_posterior,
+            bias_prior=bias_prior,
+            kl_divergence_fn=kl_divergence_fn,
+            layer_fn=F.Conv2d,
+            stride=stride,
+            padding=padding,
+            dilation=dilation,
+            groups=groups)
 
 class LinearLRT(LRTLayer):
 
@@ -106,21 +96,19 @@ class LinearLRT(LRTLayer):
             kl_divergence_fn = kl_divergence
 
         if bias:
-            super(LinearLRT, self).__init__(
-                weight_posterior=mean_field_normal_initializer((out_features, in_features), posterior["loc"], posterior["ro"]),
-                weight_prior=prior,
-                bias_posterior=mean_field_normal_initializer(out_features, posterior["loc"], posterior["ro"]),
-                bias_prior=prior,
-                kl_divergence_fn=kl_divergence_fn,
-                layer_fn=F.linear)
+            bias_posterior = mean_field_normal_initializer(out_features, posterior["loc"], posterior["ro"])
+            bias_prior = prior
         else:
-            super(LinearLRT, self).__init__(
-                weight_posterior=mean_field_normal_initializer((out_features, in_features), posterior["loc"], posterior["ro"]),
-                weight_prior=prior,
-                bias_posterior=None,
-                bias_prior=None,
-                kl_divergence_fn=kl_divergence_fn,
-                layer_fn=F.linear)
+            bias_posterior, bias_prior = None, None
+
+        super(LinearLRT, self).__init__(
+            weight_posterior=mean_field_normal_initializer((out_features, in_features), posterior["loc"], posterior["ro"]),
+            weight_prior=prior,
+            bias_posterior=bias_posterior,
+            bias_prior=bias_prior,
+            kl_divergence_fn=kl_divergence_fn,
+            layer_fn=F.linear)
+
 
 class Conv2dLRT(LRTLayer):
 
@@ -146,26 +134,19 @@ class Conv2dLRT(LRTLayer):
             kl_divergence_fn = kl_divergence
 
         if bias:
-            super(Conv2dLRT, self).__init__(
-                weight_posterior=mean_field_normal_initializer((out_channels, in_channels // groups, kernel_size[0], kernel_size[1]), posterior["loc"], posterior["ro"]),
-                weight_prior=prior,
-                bias_posterior=mean_field_normal_initializer(out_channels, posterior["loc"], posterior["ro"]),
-                bias_prior=prior,
-                kl_divergence_fn=kl_divergence_fn,
-                layer_fn=F.Conv2d,
-                stride=stride,
-                padding=padding,
-                dilation=dilation,
-                groups=groups)
+            bias_posterior = mean_field_normal_initializer(out_channels, posterior["loc"], posterior["ro"])
+            bias_prior = prior
         else:
-            super(Conv2dLRT, self).__init__(
-                weight_posterior=mean_field_normal_initializer((out_channels, in_channels // groups, kernel_size[0], kernel_size[1]), posterior["loc"], posterior["ro"]),
-                weight_prior=prior,
-                bias_posterior=None,
-                bias_prior=None,
-                kl_divergence_fn=kl_divergence_fn,
-                layer_fn=F.Conv2d,
-                stride=stride,
-                padding=padding,
-                dilation=dilation,
-                groups=groups)
+            bias_posterior, bias_prior = None, None
+
+        super(Conv2dLRT, self).__init__(
+            weight_posterior=mean_field_normal_initializer((out_channels, in_channels // groups, kernel_size[0], kernel_size[1]), posterior["loc"], posterior["ro"]),
+            weight_prior=prior,
+            bias_posterior=bias_posterior,
+            bias_prior=bias_prior,
+            kl_divergence_fn=kl_divergence_fn,
+            layer_fn=F.Conv2d,
+            stride=stride,
+            padding=padding,
+            dilation=dilation,
+            groups=groups)
