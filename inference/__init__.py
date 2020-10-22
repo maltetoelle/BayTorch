9b8043469eb6982a6c4eb24b99de4b1b6bd1f4e2
Malte Tölle <malte.toelle@gmail.com>
2020-10-22 10:07:41 +0200

    percentage pruning ffg

--- inference/__init__.py
+++ inference/__init__.py
@@ -1,9 +1,9 @@
 from .classification import ClassificationTrainer, Predictor
 from .losses import NLLLoss2d, uceloss
-from .utils import uncert_regression_gal, uncert_classification_kwon, accuracy, get_beta, ThresholdPruning, prune_weights
+from .utils import uncert_regression_gal, uncert_classification_kwon, accuracy, get_beta, ThresholdPruning, prune_weights, PercentagePruningFFG
 
 all = [
     "ClassificationTrainer", "Predictor", "NLLLoss2d", "uceloss",
     "uncert_regression_gal", "uncert_classification_kwon", "accuracy",
-    "get_beta", "ThresholdPruning", "prune_weights"
+    "get_beta", "ThresholdPruning", "prune_weights", "PercentagePruningFFG"
 ]
